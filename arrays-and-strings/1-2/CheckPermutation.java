ce1b29464e5e5517dd97f98349ba4d941d38d344
Ziyang Jin <ziyangjin@Ziyangs-MBP.hitronhub.home>
2017-02-06 23:03:37 -0800

    solution to 1.2:Check Permutation

---
+++ arrays-and-strings/1-2/CheckPermutation.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class CheckPermutation {
+    public static boolean check(String first, String second) {
+    	if (first.length() == second.length()) {
+    		char[] firstArray = first.toCharArray();
+    		char[] secondArray = second.toCharArray();
+    		Arrays.sort(firstArray);
+    		Arrays.sort(secondArray);
+    		for (int i = 0; i < firstArray.length; i++) {
+    			if (firstArray[i] != secondArray[i]) {
+    				return false;
+    			}
+    		}
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+}
---
+++ arrays-and-strings/1-2/CheckPermutationTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class CheckPermutationTest {
+    @Test
+    public void sameString() {
+        String first = "abcde";
+        String second = "bdcae";
+        assertTrue(CheckPermutation.check(first, second));
+    }
+    
+    @Test
+    public void differentString() {
+    	String first = "asdfg";
+    	String second = "adfgw";
+    	assertFalse(CheckPermutation.check(first, second));
+    }
+    
+    @Test
+    public void differentLength() {
+    	String first = "one";
+    	String second = "three";
+    	assertFalse(CheckPermutation.check(first, second));
+    }
+    
+    @Test
+    public void twoEmtpyStrings() {
+    	String first = "";
+    	String second = "";
+    	assertTrue(CheckPermutation.check(first, second));
+    }
+    
+    @Test
+    public void twoSpaceStrings() {
+    	String first = " ";
+    	String second = " ";
+    	assertTrue(CheckPermutation.check(first, second));
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void oneNullString() {
+    	String first = null;
+    	String second = "abc";
+    	CheckPermutation.check(first, second);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void twoNullStrings() {
+    	String first = null;
+    	String second = null;
+    	CheckPermutation.check(first, second);
+    }
+}
