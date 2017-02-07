6f4cdd331cf9c994e20220ddf0bc90741aefc4a7
Ziyang Jin <ziyangjin@Ziyangs-MBP.hitronhub.home>
2017-02-06 22:11:28 -0800

    solution to 1.1: Is Unique

---
+++ arrays-and-strings/1-1/IsUnique.java
@@ -0,0 +1,21 @@
+public class IsUnique {
+
+    public static  boolean checkUniqueness(String s) {
+    	for (int i = 0; i < s.length(); i++) {
+    		if (include(s.substring(i+1), s.charAt(i))) {
+    			return false;
+    		}
+    	}
+        return true;
+    }
+    
+    private static boolean include(String s, char c) {
+    	for (int i = 0; i < s.length(); i++) {
+    		if (c == s.charAt(i)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+
+}
