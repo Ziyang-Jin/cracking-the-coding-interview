a8ad402be14bc9f0b23817d1dac714e03762268d
Ziyang Jin <ziyang.jin.master@gmail.com>
2017-02-15 22:42:49 -0800

    fix memory leak in code

--- linked-lists/2-1/remove_dups.c
+++ linked-lists/2-1/remove_dups.c
@@ -21,7 +21,7 @@ void printList(struct Node* head) {
     }
 }
 
-void removeAll(int val, struct Node* head) {
+void removeAllSkipHead(int val, struct Node* head) {
     if (head) {
 	struct Node* prev = head;
         struct Node* ref = head->next;
@@ -40,7 +40,7 @@ void removeAll(int val, struct Node* head) {
 void removeDups(struct Node* head) {
     struct Node* ref = head;
     while (ref) {
-        removeAll(ref->val, ref);
+        removeAllSkipHead(ref->val, ref);
 	ref = ref->next;
     }
     printList(head);
@@ -53,13 +53,34 @@ struct Node* makeNode(int val) {
     return node;
 }
 
-int main(void) {
+struct Node* makeTestList() {
+    struct Node* node0 = makeNode(1);
     struct Node* node1 = makeNode(1);
     struct Node* node2 = makeNode(2);
     struct Node* node3 = makeNode(3);
-    struct Node* head = node1;
+    struct Node* node4 = makeNode(3);
+    struct Node* node5 = makeNode(2);
+    struct Node* head = node0;
+    node0->next = node1;
     node1->next = node2;
     node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
     printList(head);
-    removeDups(head);
+    return head;
+}
+
+void destroyList(struct Node* head) {
+    struct Node* ref = head;
+    while (head) {
+        head = head->next;
+	free(ref);
+	ref = head;
+    }
+}
+
+int main(void) {
+    struct Node* list = makeTestList();
+    removeDups(list);
+    destroyList(list);
 }
