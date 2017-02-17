35bfe37ea52c8dc2c5c0d60c87a5df77d32ee006
Ziyang Jin <ziyang.jin.master@gmail.com>
2017-02-16 20:59:19 -0800

    solution to 2.5: Sum Lists

---
+++ linked-lists/2-5/sum_lists.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../linked_list.h"
+
+int getNumber(struct Node* list) {
+    int result = 0;
+    while (list) {
+        result = result * 10 + list->val;
+	list = list->next;
+    }
+    return result;
+}
+
+int sumLists(struct Node* list1, struct Node* list2) {
+    return getNumber(list1) + getNumber(list2);
+}
+
+int getNumberBackward(struct Node* list) {
+    int times = 1;
+    int result = 0;
+    while (list) {
+        result += list->val * times;
+	times *= 10;
+	list = list->next;
+    }
+    return result;
+}
+
+int sumListsBackward(struct Node* list1, struct Node* list2) {
+    return getNumberBackward(list1) + getNumberBackward(list2);
+}
+
+int main(void) {
+    struct Node* list1 = generateList(3);
+    struct Node* list2 = generateList(2);
+    int sum = sumLists(list1, list2);
+    printf("forward: list1 + list2 = %d\n", sum);
+    sum = sumListsBackward(list1, list2);
+    printf("backward: list1 + list2 = %d\n", sum);
+}
