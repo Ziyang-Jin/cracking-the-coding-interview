e21748a5fad69ec774fb5499231996ca72c2bf00
Ziyang Jin <ziyang.jin.master@gmail.com>
2017-02-21 22:46:09 -0800

    solution to 3.6: Animal Shelter

---
+++ stacks-and-queues/3-6/AnimalShelter.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class AnimalShelter {
+    LinkedList<Animal> ll = new LinkedList<Animal>();
+    
+    public void enqueue(Animal a) {
+    	ll.add(a);
+    }
+    
+    public Animal dequeueAny() {
+    	return ll.removeFirst();
+    }
+    
+    public Animal dequeueCat() {
+    	return dequeueKind(Animal.CAT);
+    }
+    
+    public Animal dequeueDog() {
+    	return dequeueKind(Animal.DOG);
+    }
+    
+    private Animal dequeueKind(Animal kind) {
+    	LinkedList<Animal> temp = new LinkedList<Animal>();
+    	Animal a = ll.removeFirst();
+    	while (a != kind) {
+    		temp.add(a);
+    		a = ll.removeFirst();
+    	}
+    	temp.addAll(ll);
+    	ll = temp;
+    	return a;
+    }
+}
